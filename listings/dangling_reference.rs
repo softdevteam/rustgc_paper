14171e331c665814af71317d971970b0b2229e14
Jake Hughes <jh@jakehughes.uk>
2024-08-08 22:59:12 +0100

    Update FSA examples

--- listings/dangling_reference.rs
+++ listings/dangling_reference.rs
@@ -7,12 +7,12 @@ struct Node<'a> { value: &'a u8, nbr: Option<Gc<RefCell<Node<'a>>>> }
 impl<'a> Drop for Node<'a> { fn drop(&mut self) { println!("drop {}", self.value); } }
 
 fn main() {
-    {
-        let b: Box<u8> = Box::new(123);
-        let gc1 = Gc::new(Node{value: &*b, nbr: None});
-    } // b is dropped and its memory deallocated
+ {
+   let b: Box<u8> = Box::new(123);
+   let gc1 = Gc::new(Node{value: &*b, nbr: None});
+ } // b is dropped and its memory deallocated
 
-    // causes `gc::drop` finaliser to run which
-    // holds a reference to `b`.
-    force_gc();
+ // causes `gc::drop` finaliser to run which
+ // holds a reference to `b`.
+ force_gc();
 }
