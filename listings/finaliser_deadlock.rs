42d13ffd1cc5a1dd1c405724bdf62e4161295b53
Laurence Tratt <laurie@tratt.net>
2024-10-11 12:35:04 +0100

    Start editing the "destructors and finaliser thread" section.

--- listings/finaliser_deadlock.rs
+++ listings/finaliser_deadlock.rs
@@ -5,28 +5,16 @@ use std::cell::RefCell;
 use std::sync::Mutex;
 fn force_gc() { std::gc::GcAllocator::force_gc() }
 
-struct Node { value: Rc<Mutex<u8>>, nbr: Option<Gc<RefCell<Node>>> }
+struct GcNode { value: Rc<Mutex<u8>>, nbr: Option<Gc<RefCell<Node>>> }
 
-impl Drop for Node {
-    fn drop(&mut self) {
-        let d = self.value.lock().unwrap();
-        println!("drop {}", d);
-    } // d is dropped
+impl Drop for GcNode {
+  fn drop(&mut self) { println!("drop {}", self.value.lock().unwrap()); }
 }
 
 fn main() {
-    // Create a reference counted counter object protected by a mutex.
-    let counter = Rc::new(Mutex::new(0));
-
-    {
-        let gc1 = Gc::new(Node { value: Rc::clone(&counter), nbr: None });
-    }
-
-    force_gc();
-
-    // If the finalizer for the `Gc<Node>` is asynchronously called
-    // while `counter.lock()` below is already acquired, a deadlock will occur.
-    let r1 = counter.lock().unwrap();
-
-    assert_eq!(*r1, 0);
+  let counter = Rc::new(Mutex::new(0));
+  { let _ = Gc::new(GcNode { value: Rc::clone(&counter), nbr: None }); }
+  let r1 = counter.lock().unwrap(); // Can deadlock
+  force_gc();
+  assert_eq!(*r1, 0);
 }
