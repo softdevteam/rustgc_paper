6b4406f84a6a41f2df05f3419e4943de03b2057d
Laurence Tratt <laurie@tratt.net>
2024-10-11 17:36:10 +0100

    Edit more of the FSA section.

--- listings/finalisation_cycle.rs
+++ listings/finalisation_cycle.rs
@@ -6,12 +6,9 @@ struct GcNode { value: u8, nbr: Option<Gc<RefCell<GcNode>>> }
 impl Drop for GcNode {
   fn drop(&mut self) { self.value = 0; println!("{}", self.nbr.unwrap().value); }
 }
-fn main()  {
+fn f()  {
   let mut gc1 = Gc::new(RefCell::new(GcNode{value: 1, nbr: None}));
   let gc2 = Gc::new(RefCell::new(GcNode{value: 2, nbr: None}));
   gc1.borrow_mut().nbr = Some(gc2);
   gc2.borrow_mut().nbr = Some(gc1);
-  gc1 = gc2;
-  force_gc();
-  // Assume both finalisers run at this point
 }
